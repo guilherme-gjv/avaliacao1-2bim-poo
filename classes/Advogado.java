1329495bcb86e536a54f5d2a409488ae8dd029e9
guilherme-gjv <83377948+guilherme-gjv@users.noreply.github.com>
2021-08-21 14:12:29 -0300

    chore: constructor, method structures implemented

--- classes/Advogado.java
+++ classes/Advogado.java
@@ -1,5 +1,30 @@
 package classes;
 
 public class Advogado {
-    
+
+    //nome, CPF, telefone, data que foi admitido e o departamento que atua 
+    Advogado(){
+        getDados();
+    }
+
+    void getDados() {
+        String nome, data_admitido;
+        int cpf, telefone;
+        Departamento departamento;
+
+        System.out.println("Nome do advogado: ");
+        System.out.println("Número do CPF: ");
+        System.out.println("Número do telefone: ");
+        System.out.println("Data que foi admitido: ");
+        System.out.println("Departamento: ");
+    }
+
+    void exibir() {
+
+    }
+
+    void getDepartamento() {
+
+    }
+    // pode ser usado tanto no construtor quanto para trocar o departamento
 }
