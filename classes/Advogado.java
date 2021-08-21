666eb913a5a10eb1e4125f8c3528ebcf9e7eacea
guilherme-gjv <83377948+guilherme-gjv@users.noreply.github.com>
2021-08-21 16:18:21 -0300

    chore: method's content implemented

--- classes/Advogado.java
+++ classes/Advogado.java
@@ -1,30 +1,48 @@
 package classes;
 
+import java.util.Scanner;
+
 public class Advogado {
 
-    //nome, CPF, telefone, data que foi admitido e o departamento que atua 
-    Advogado(){
+    // nome, CPF, telefone, data que foi admitido e o departamento que atua
+    Advogado() {
         getDados();
     }
 
-    void getDados() {
-        String nome, data_admitido;
-        int cpf, telefone;
-        Departamento departamento;
+    Scanner ler = new Scanner(System.in);
+    String nome, data_admitido;
+    int cpf, telefone;
+    Departamento departamento;
 
+    void getDados() {
         System.out.println("Nome do advogado: ");
+        nome = ler.nextLine();
         System.out.println("Número do CPF: ");
+        cpf = ler.nextInt();
         System.out.println("Número do telefone: ");
+        telefone = ler.nextInt();
         System.out.println("Data que foi admitido: ");
-        System.out.println("Departamento: ");
+        data_admitido = ler.nextLine();
+        getDepartamento();
+
     }
 
-    void exibir() {
-
+    void switchDepartamento() {
+        getDepartamento();
     }
 
     void getDepartamento() {
-
+        System.out.println("Digite o Departamento: ");
+        departamento = new Departamento();
     }
+
     // pode ser usado tanto no construtor quanto para trocar o departamento
+    void exibir() {
+        System.out.println("Exibindo dados do Advogado");
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Telefone: " + telefone);
+        System.out.println("Data de admissão: " + data_admitido);
+        System.out.println("Departamento: " + departamento);
+    }
 }
