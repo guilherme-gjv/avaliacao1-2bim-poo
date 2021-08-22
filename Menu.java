fc1c2c42004db4007d530a767260f4ec12768d52
guilherme-gjv <83377948+guilherme-gjv@users.noreply.github.com>
2021-08-21 21:03:34 -0300

    feat: added Menu class and methods

---
+++ Menu.java
@@ -0,0 +1,27 @@
+
+public class Menu {
+
+    //Exibir Advogados, Livros, Empréstimos
+    void exibirAdLiEm(){
+
+    }
+
+    void emprestimo(){
+
+    }
+
+    void devolver(){
+    //se for o caso, exibir o valor a pagar
+
+    }
+
+    //trocar o departamento do advogado
+    //
+    void trocarDepAdvogado(){
+
+    }
+    //Exibir os títulos dos livros que estão emprestados de uma área específica (solicitar o nome da área)
+    void exibirLivrosEmprestados(){
+        
+    }
+}
