322ef74e08b974072e38e00511c2fa6bc294b43a
guilherme-gjv <83377948+guilherme-gjv@users.noreply.github.com>
2021-08-23 16:23:13 -0300

    chore: menu's choice method implemented

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String args[]) {
+        Menu m1 = new Menu();
+        //teste!
+    }
+
+}
--- Menu.java
+++ Menu.java
@@ -1,27 +1,76 @@
+import java.util.Scanner;
 
 public class Menu {
 
-    //Exibir Advogados, Livros, Empréstimos
-    void exibirAdLiEm(){
+    Scanner ler = new Scanner(System.in);
+
+    Menu() {
+        exibirMenu();
+    }
+
+    void exibirMenu() {
+        int escolha;
+        System.out.println("\tIFAL \n---Instituição Federal de Advocacia Legal----");
+        System.out.println(">Gerenciamento de Livros\nDigite...");
+        System.out.println("1. Exibir Advogados, Livros e Empréstimos.");
+        System.out.println("2. Empréstimo");
+        System.out.println("3. Devolver");
+        System.out.println("4. Trocar Departamento de um Advogado");
+        System.out.println("5. Exibir livros emprestados.");
+        System.out.println("0. Sair.");
+
+        escolha = ler.nextInt();
+        // provisorio
+
+        switch (escolha) {
+            case 1:
+                exibirAdLiEm();
+                break;
+
+            case 2:
+                emprestimo();
+                break;
+
+            case 3:
+                devolver();
+                break;
+
+            case 4:
+                trocarDepAdvogado();
+                break;
+
+            case 5:
+                exibirLivrosEmprestados();
+                break;
+
+            case 0:
+                System.exit(1);
+                break;
+        }
 
     }
 
-    void emprestimo(){
+    // Exibir Advogados, Livros, Empréstimos
+    void exibirAdLiEm() {
 
     }
 
-    void devolver(){
-    //se for o caso, exibir o valor a pagar
+    void emprestimo() {
 
     }
 
-    //trocar o departamento do advogado
-    //
-    void trocarDepAdvogado(){
+    void devolver() {
+        // se for o caso, exibir o valor a pagar
+    }
+
+    // trocar o departamento do advogado
+    void trocarDepAdvogado() {
 
     }
-    //Exibir os títulos dos livros que estão emprestados de uma área específica (solicitar o nome da área)
-    void exibirLivrosEmprestados(){
-        
+
+    // Exibir os títulos dos livros que estão emprestados de uma área específica
+    // (solicitar o nome da área)
+    void exibirLivrosEmprestados() {
+
     }
 }
