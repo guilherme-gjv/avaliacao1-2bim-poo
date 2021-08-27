642a92be7f9abaacd397e8fdeb15d22e6f22439e
duardoqueiroz <eduardoldq1@gmail.com>
2021-08-27 20:59:48 -0300

    feat: access modifiers

--- Departamento.java
+++ Departamento.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class Departamento {
-    String nome, responsavel;
-    int quantidade_processos;
-    Scanner ler = new Scanner(System.in);
+    private String nome, responsavel;
+    private int quantidade_processos;
+    private Scanner ler = new Scanner(System.in);
 
-    Departamento(String nome, String responsavel, int quantidade_processos) {
+    public Departamento(String nome, String responsavel, int quantidade_processos) {
         this.nome = nome;
         this.responsavel = responsavel;
         this.quantidade_processos = quantidade_processos;
     }
 
-    Departamento() {
+    public Departamento() {
         System.out.println("Informe os dados do Departamento");
         System.out.println("Nome: ");
         nome = ler.nextLine();
@@ -22,11 +22,17 @@ public class Departamento {
 
     }
 
-    void exibir() {
+    // Método para exibir dados do departamento
+    public void exibir() {
         System.out.println("\n-----Dados do Departamento-----\n");
         System.out.println("Nome do departamento: " + nome);
         System.out.println("Responsavel: " + responsavel);
         System.out.println("Quantidade de Processos> " + quantidade_processos);
         System.out.println("\n----------------------------------\n");
+    }
+
+    // ---------MÉTODO GET------------
+    public String getNome() {
+        return nome;
     }
 }
