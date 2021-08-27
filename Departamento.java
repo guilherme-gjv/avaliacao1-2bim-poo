0f8b5385bf9b5fb937eed957fc15317792bd0064
duardoqueiroz <eduardoldq1@gmail.com>
2021-08-27 13:42:10 -0300

    fix: display method errors

--- Departamento.java
+++ Departamento.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
+
 public class Departamento {
     String nome, responsavel;
     int quantidade_processos;
     Scanner ler = new Scanner(System.in);
-    Departamento(String nome, String responsavel, int quantidade_processos){
+
+    Departamento(String nome, String responsavel, int quantidade_processos) {
         this.nome = nome;
         this.responsavel = responsavel;
         this.quantidade_processos = quantidade_processos;
     }
-    Departamento(){
+
+    Departamento() {
         System.out.println("Informe os dados do Departamento");
         System.out.println("Nome: ");
         nome = ler.nextLine();
@@ -16,13 +19,14 @@ public class Departamento {
         responsavel = ler.nextLine();
         System.out.println("Quantidade de Processos: ");
         quantidade_processos = ler.nextInt();
-        
+
     }
-    
-    void exibir(){
-        System.out.println("Dados do Departamento: ");
-        System.out.println("Nome: "+ responsavel);
-        System.out.println("Responsavel: "+ responsavel);
-        System.out.println("Quantidade de Processos> "+ quantidade_processos);
+
+    void exibir() {
+        System.out.println("\n-----Dados do Departamento-----\n");
+        System.out.println("Nome do departamento: " + nome);
+        System.out.println("Responsavel: " + responsavel);
+        System.out.println("Quantidade de Processos> " + quantidade_processos);
+        System.out.println("\n----------------------------------\n");
     }
 }
